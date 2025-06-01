deeadc0fbc6bf45a46f685362a12cd2fcee8df4e
KonradS <konrads733@gmail.com>
2025-06-01 11:58:25 +0200

    Update Sqlite database structure to include products

--- Web_Shop.Tests/Web_Shop.Tests_InMemoryDB/SqliteDatabaseFixture.cs
+++ Web_Shop.Tests/Web_Shop.Tests_InMemoryDB/SqliteDatabaseFixture.cs
@@ -31,14 +31,25 @@ namespace Web_Shop.Tests_InMemoryDB
             {
                 using var viewCommand = context.Database.GetDbConnection().CreateCommand();
                 viewCommand.CommandText = @"
-CREATE VIEW AllResources AS
-SELECT Name
-FROM Customer;";
+                CREATE VIEW AllCustomers AS
+                SELECT Name
+                FROM Customer;";
+                viewCommand.ExecuteNonQuery();
+
+                viewCommand.CommandText = @"
+                CREATE VIEW AllProducts AS
+                SELECT Name
+                FROM Product;";
                 viewCommand.ExecuteNonQuery();
             }
             context.AddRange(
                 new Customer { IdCustomer = 1, Name = "Michał", Surname = "Styś", Email = "michal.stys@gmail.com", PasswordHash = BC.HashPassword("Test111") },
                 new Customer { IdCustomer = 2, Name = "Jan", Surname = "Kowalski", Email = "jan.kowalski@o2.pl", PasswordHash = BC.HashPassword("Test222") });
+            context.SaveChanges();
+
+            context.AddRange(
+                new Product { IdProduct = 1, Name = "Torba na laptopa 15.6 cala", Description = "Stylowa i wytrzymała torba z wyściełaną przegrodą na laptopa, idealna do pracy i podróży.", Price = 79.90M, Sku = "AKC_LAP_TOR02" },
+                new Product { IdProduct = 2, Name = "Dron wojskowy", Description = "Dron wojskowy do rejestrowania zdjęć z powietrza.", Price = 10000, Sku = "AKC_DRN_WOJ01" });
             context.SaveChanges();
 
             return context;

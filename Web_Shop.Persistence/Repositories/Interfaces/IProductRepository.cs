47a6064e0bba3fd29fd8b633880971fb26e6a834
KonradS <konrads733@gmail.com>
2025-06-01 11:56:04 +0200

    Add Product Repository interface

---
+++ Web_Shop.Persistence/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using WWSI_Shop.Persistence.MySQL.Model;
+
+namespace Web_Shop.Persistence.Repositories.Interfaces
+{
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+        Task<bool> NameExistsAsync(string name);
+        Task<bool> IsNameEditAllowedAsync(string name, ulong id);
+        Task<Product?> GetByNameAsync(string name);
+        Task<bool> SkuExistsAsync(string sku);
+    }
+}

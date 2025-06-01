76d8af43694e0a820aef2a80c25353bc9a7ef8f2
KonradS <konrads733@gmail.com>
2025-06-01 11:59:04 +0200

    Add tests for in memory product repository and service

---
+++ Web_Shop.Tests/Web_Shop.Tests_InMemoryDB/SqliteInMemoryProductRepositoryTest.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Web_Shop.Persistence.Repositories;
+using WWSI_Shop.Persistence.MySQL.Context;
+using WWSI_Shop.Persistence.MySQL.Model;
+
+namespace Web_Shop.Tests_InMemoryDB
+{
+    public class SqliteInMemoryProductRepositoryTest : IDisposable
+    {
+        private readonly SqliteDatabaseFixture _databaseFixture;
+        public SqliteInMemoryProductRepositoryTest()
+        {
+            _databaseFixture = new SqliteDatabaseFixture();
+        }
+        public void Dispose()
+        {
+            _databaseFixture.Dispose();
+        }
+
+        [Fact]
+        public async Task ProductRepository_SkuExistsAsync_ReturnsTrue()
+        {
+            using var context = _databaseFixture.CreateContext();
+
+            var productRepository = new ProductRepository(context);
+
+            var skuExists = await productRepository.SkuExistsAsync("AKC_LAP_TOR02");
+
+            Assert.True(skuExists);
+        }
+
+        [Fact]
+        public async Task ProductRepository_SkuExistsAsync_ReturnsFalse()
+        {
+            using var context = _databaseFixture.CreateContext();
+
+            var productRepository = new ProductRepository(context);
+
+            var skuExists = await productRepository.SkuExistsAsync("GGG_FFF_HHH01");
+
+            Assert.False(skuExists);
+        }
+
+        [Fact]
+        public async Task ProductRepository_NameExistsAsync_ReturnsTrue()
+        {
+            using var context = _databaseFixture.CreateContext();
+
+            var productRepository = new ProductRepository(context);
+
+            var nameExists = await productRepository.NameExistsAsync("Torba na laptopa 15.6 cala");
+
+            Assert.True(nameExists);
+        }
+
+        [Fact]
+        public async Task ProductRepository_NameExistsAsync_ReturnsFalse()
+        {
+            using var context = _databaseFixture.CreateContext();
+
+            var productRepository = new ProductRepository(context);
+
+            var nameExists = await productRepository.NameExistsAsync("Dron wojskowy niebieski");
+
+            Assert.False(nameExists);
+        }
+
+        [Fact]
+        public async Task ProductRepository_IsNameEditAllowedAsync_ReturnsTrue()
+        {
+            using var context = _databaseFixture.CreateContext();
+
+            var productRepository = new ProductRepository(context);
+
+            var nameEditAllowed = await productRepository.IsNameEditAllowedAsync("Torba na laptopa 15.6 cala", 1);
+
+            Assert.True(nameEditAllowed);
+        }
+
+        [Fact]
+        public async Task ProductRepository_IsNameEditAllowedAsync_ReturnsFalse()
+        {
+            using var context = _databaseFixture.CreateContext();
+
+            var productRepository = new ProductRepository(context);
+
+            var nameEditAllowed = await productRepository.IsNameEditAllowedAsync("Dron wojskowy", 1);
+
+            Assert.False(nameEditAllowed);
+        }
+
+        [Fact]
+        public async Task ProductRepository_GetByNameAsync_Exists()
+        {
+            using var context = _databaseFixture.CreateContext();
+
+            var productRepository = new ProductRepository(context);
+
+            var product = await productRepository.GetByNameAsync("Torba na laptopa 15.6 cala");
+
+            Assert.Equal("Torba na laptopa 15.6 cala", product.Name);
+            Assert.Equal("Stylowa i wytrzymała torba z wyściełaną przegrodą na laptopa, idealna do pracy i podróży.", product.Description);
+        }
+
+        [Fact]
+        public async Task ProductRepository_GetByNameAsync_NotExist()
+        {
+            using var context = _databaseFixture.CreateContext();
+
+            var productRepository = new ProductRepository(context);
+
+            var product = await productRepository.GetByNameAsync("Dron wojskowy niebieski");
+
+            Assert.Null(product);
+        }
+    }
+}
---
+++ Web_Shop.Tests/Web_Shop.Tests_InMemoryDB/SqliteInMemoryProductServiceTest.cs
@@ -0,0 +1,102 @@
+﻿using Castle.Core.Logging;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Sieve.Models;
+using Sieve.Services;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Web_Shop.Application.CustomQueries;
+using Web_Shop.Application.DTOs;
+using Web_Shop.Application.Mappings;
+using Web_Shop.Application.Mappings.PropertiesMappings;
+using Web_Shop.Application.Services;
+using Web_Shop.Persistence.Repositories;
+using Web_Shop.Persistence.Repositories.Interfaces;
+using Web_Shop.Persistence.UOW;
+using Web_Shop.Persistence.UOW.Interfaces;
+using Web_Shop.Tests.Common.Sieve;
+using WWSI_Shop.Persistence.MySQL.Context;
+using WWSI_Shop.Persistence.MySQL.Model;
+
+namespace Web_Shop.Tests_InMemoryDB
+{
+    public class SqliteInMemoryProductServiceTest : IDisposable
+    {
+        private readonly SqliteDatabaseFixture _databaseFixture;
+
+        private readonly Mock<ILogger<Product>> _loggerMock;
+
+        private readonly SieveProcessor _processor;
+        private readonly SieveOptionsAccessor _optionsAccessor;
+
+        public SqliteInMemoryProductServiceTest()
+        {
+            _databaseFixture = new SqliteDatabaseFixture();
+
+            _loggerMock = new Mock<ILogger<Product>>();
+
+            _optionsAccessor = new SieveOptionsAccessor();
+
+            _processor = new ApplicationSieveProcessor(_optionsAccessor,
+                new SieveCustomSortMethods(),
+                new SieveCustomFilterMethods());
+        }
+        public void Dispose()
+        {
+            _databaseFixture.Dispose();
+        }
+
+        [Fact]
+        public async Task ProductService_CreateNewProductAsync_ReturnsTrue()
+        {
+            {
+                using var context = _databaseFixture.CreateContext();
+
+                var unitOfWork = new UnitOfWork(context);
+
+                var productService = new ProductService(_loggerMock.Object, _processor, _optionsAccessor, unitOfWork);
+
+                var addUpdateProductDTO = new AddUpdateProductDTO()
+                {
+                    Name = "TestName",
+                    Description = "TestDescription",
+                    Price = 5,
+                    Sku = "AAA_BBB_ABC01"
+                };
+
+                var verifyResult = await productService.CreateNewProductAsync(addUpdateProductDTO);
+
+                Assert.True(verifyResult.IsSuccess);
+                Assert.Equal(System.Net.HttpStatusCode.OK, verifyResult.StatusCode);
+                Assert.Equal("AAA_BBB_ABC01", verifyResult.entity!.Sku);
+            }
+        }
+
+        [Fact]
+        public async Task ProductService_SearchAsync_ReturnsTrue()
+        {
+            using var context = _databaseFixture.CreateContext();
+
+            var unitOfWork = new UnitOfWork(context);
+
+            var productService = new ProductService(_loggerMock.Object, _processor, _optionsAccessor, unitOfWork);
+
+            var model = new SieveModel
+            {
+                Filters = "Name@=Dron"
+            };
+
+            var searchResult = await productService.SearchAsync(model, resultEntity => DomainToDtoMapper.MapGetSingleProductDTO(resultEntity));
+
+            Assert.True(searchResult.IsSuccess);
+            Assert.Equal(1, searchResult.entityList!.TotalItemCount);
+        }
+    }
+}
